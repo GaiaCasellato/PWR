ea60c26c187cef0ec2a6ee96b42582bee9599ac4
GaiaCasellato <162143185+GaiaCasellato@users.noreply.github.com>
2024-09-24 14:35:18 +0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,3 @@
+<!--Footer della pagina con indicazioni di copyright, contatti dell'autore (email) e indicazioni sulla pagina corrente-->
+<p>Copyright &copy; 2024 <a href="mailto:s287445@studenti.polito.it"> Gaia Casellato</a>. Tutti i diritti riservati.
+ Pagina corrente: <?php echo basename($_SERVER['PHP_SELF']); ?></p>
